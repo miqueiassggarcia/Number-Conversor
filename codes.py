46ffb74e62b2ef9add8ce4fd876a1c189265bdde
Miquéias Garcia <mglucenalucena@gmail.com>
2023-03-10 10:35:28 -0300

    Adicionando calculo para números com ponto flutuante

--- codes.py
+++ codes.py
@@ -10,34 +10,66 @@ correspondencias = {
 def reverseString(string):
     return string[::-1]
 
-def DivideMethod(value, numeral):
+def concatenateArrayIntsInString(values):
+    result = ""
+    for i in range(len(values)):
+        if(values[i] > 9):
+            result += correspondencias[values[i]]
+        else:
+            result += str(values[i])
+    return result
+
+def divideMethod(value, numeral):
     # array de resultado das operações
     rests = []
+    if (value < 1):
+        rests.append(0)
+
+    integerValue = int(value)
+    floatValue = value - int(value)
 
     # realizando operações e invertendo para ordem correta
-    while (value > 0):
-        rests.append(value%numeral)
-        value = value//numeral
+    while (integerValue > 0):
+        rests.append(integerValue%numeral)
+        integerValue = integerValue//numeral
     rests.reverse()
 
-    result = ""
-    for i in range(len(rests)):
-        if(rests[i] > 9):
-            result += correspondencias[rests[i]]
+    result = concatenateArrayIntsInString(rests)
+
+    if(type(value) == float):
+        restsFloat = []
+
+        while(floatValue > 0):
+            floatValue *= numeral
+            restsFloat.append(int(floatValue))
+            floatValue = floatValue - int(floatValue)
+        
+        result = result + "." + concatenateArrayIntsInString(restsFloat)
+    
+    return result
+
+def powNumbers(values, init, end, numeral, step):
+    result = 0
+    for i in range(init, end, step):
+        if(values[i] >= "A" and values[i] <= "F"):
+            value = [j for j in correspondencias if correspondencias[j] == values[i]]
+            result += value[0] * pow(numeral, i)
         else:
-            result += str(rests[i])
+            result += int(values[i])*pow(numeral, i)
     return result
 
 def powMethod(value, numeral):
-    valueParse = reverseString(str(value))
-    result = 0
-    for i in range(len(valueParse)):
-        if(valueParse[i] >= "A" and valueParse[i] <= "F"):
-            value = [j for j in correspondencias if correspondencias[j] == valueParse[i]]
-            result += value[0] * pow(numeral, i)
-        else:
-            result += int(valueParse[i])*pow(numeral, i)
-    return result
+    if(type(value) == float):
+        separetaValues = str(value).split(".")
+        intergerValues = reverseString(separetaValues[0])
+        floatValues = separetaValues[1]
 
-print(DivideMethod(990129, 16))
-print(powMethod("FFAEB", 16))
+        integers = powNumbers(intergerValues, 0, len(intergerValues), numeral, 1)
+        floats = powNumbers(floatValues, -1, -(len(floatValues) + 1), numeral, -1)
+
+        result = integers + floats
+    else:
+        intergerValues = reverseString(str(value))
+        result = powNumbers(intergerValues, 0, numeral, 1)
+    
+    return result
